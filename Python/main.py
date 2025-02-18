686677fd12eb873f2fbb0f83c97e810b34eff7ad
Mitchie Steddom <jonathansteddom@Mitchies-MacBook-Pro.local>
2025-02-18 09:54:21 -0700

    Testing commands

---
+++ Python/main.py
@@ -0,0 +1,27 @@
+# Septemeber 9th - 2024
+
+print ("Hello world")
+
+x = 5
+y = 10
+print()
+print(x + y)
+
+# New changes
+
+print("Hello world")
+
+x = [5, 2, 104, 73, 9]
+z = 0
+for i in x:
+    z = z + i
+    
+print(z)
+
+x = [2, 4, 4, 1]
+z = 0
+for i in range(len(x)):
+    print("This is x: ", x[i])
+    z = z + x[i]
+
+print(z)
